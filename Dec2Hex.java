e9d5e09cb2baf9eaba7817698af7a947b0b06d10
DTUDOR200 <DTUDOR200@caledonian.ac.uk>
2019-11-11 14:56:49 +0000

    minor change

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -22,7 +22,7 @@ class Dec2Hex
          int rem, num;
 	num = Arg1;
 	String hexadecimal=""; 
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+        System.out.println("**** Converting the Decimal Value " + num + " to Hex...");
         
         while(num != 0)
         {
